93ddd6a66fbc225d6dcbb722c8486710c533248c
li3p <li3p@qq.com3p@qq.com3p@qq.com>
2023-03-08 22:51:45 +0800

    httpie: verify args by clap

--- rust/projects/httpie/src/main.rs
+++ rust/projects/httpie/src/main.rs
@@ -1,4 +1,8 @@
+use std::str::FromStr;
+
+use anyhow::{anyhow, Result};
 use clap::{Args, Parser, Subcommand};
+use reqwest::Url;
 
 #[derive(Parser, Debug)]
 #[command(name = "git")]
@@ -16,15 +20,50 @@ enum Command {
 }
 #[derive(Debug, Args)]
 struct Get {
-    url: String,
+    #[arg(value_parser=reqwest::Url::parse)]
+    url: Url,
 }
 #[derive(Debug, Args)]
 struct Post {
-    url: String,
-    body: Vec<String>,
+    #[arg(value_parser=reqwest::Url::parse)]
+    url: Url,
+    #[arg(value_parser=parse_kv_pair)]
+    body: Vec<KvPair>,
+}
+
+/// 命令行中的 key=value 可以通过 parse_kv_pair 解析成 KvPair 结构
+#[derive(Debug, Clone)]
+struct KvPair {
+    k: String,
+    v: String,
+}
+/// 当我们实现 FromStr trait 后，可以用 str.parse() 方法将字符串解析成 KvPair
+impl FromStr for KvPair {
+    type Err = anyhow::Error;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        // 使用 = 进行 split，这会得到一个迭代器
+        let mut split = s.split("=");
+        let err = || anyhow!(format!("Failed to parse {}", s));
+        Ok(Self {
+            // 从迭代器中取第一个结果作为 key，迭代器返回 Some(T)/None
+            // 我们将其转换成 Ok(T)/Err(E)，然后用 ? 处理错误
+            k: (split.next().ok_or_else(err)?).to_string(),
+            // 从迭代器中取第二个结果作为 value
+            v: (split.next().ok_or_else(err)?).to_string(),
+        })
+    }
+}
+/// 因为我们为 KvPair 实现了 FromStr，这里可以直接 s.parse() 得到 KvPair
+fn parse_kv_pair(s: &str) -> Result<KvPair> {
+    Ok(s.parse()?)
 }
 
 fn main() {
     let opts = Cli::parse();
-    println!("Hello, world! {:?}", opts);
+    println!("Hello, world! {:?}", opts.command);
+
+    match opts.command {
+        Command::Get(args) => println!("{:?}", args.url.to_string()),
+        Command::Post(args) => println!("{:?}", args.url.to_string()),
+    }
 }
