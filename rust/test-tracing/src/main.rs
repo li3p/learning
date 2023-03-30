4d8fed8b309b855e641abc181daad399d16ac3d1
li3p <li3p@qq.com>
2023-03-30 19:37:54 +0800

    1st version: output basic info, without fields

---
+++ rust/test-tracing/src/main.rs
@@ -0,0 +1,10 @@
+use tracing::info;
+use tracing_subscriber::prelude::*;
+
+mod custom_layer;
+use custom_layer::CustomLayer;
+
+fn main() {
+    tracing_subscriber::registry().with(CustomLayer).init();
+    info!(a_bool = true, answer = 42, message = "Hello, world!");
+}
