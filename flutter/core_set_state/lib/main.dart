cdefcc9a6cda9cbeae919e0383867e30c2d5099b
li3p <li3p@qq.com>
2023-06-03 18:28:26 +0800

    state cross pages: by Navigagor

--- flutter/core_set_state/lib/main.dart
+++ flutter/core_set_state/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:core_set_state/my_stateful_widget.dart';
+import 'package:core_set_state/page_two.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -56,17 +57,20 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _counter = 10;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void _navigate() async {
+    var value = await Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => PageTwo(counter: _counter),
+      ),
+    );
+
+    if (value != null) {
+      setState(() {
+        _counter = value;
+      });
+    }
   }
 
   @override
@@ -118,7 +122,7 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: _navigate,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
