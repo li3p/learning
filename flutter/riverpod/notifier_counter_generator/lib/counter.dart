28575fec3cc70fa7986cf15d1dae3d41aa3cfd66
li3p <li3p@qq.com>
2023-06-04 19:29:12 +0800

    Notifier Provider: generator method

---
+++ flutter/riverpod/notifier_counter_generator/lib/counter.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'counter.g.dart';
+
+@riverpod
+class Counter extends _$Counter {
+  @override
+  int build() {
+    return 250;
+  }
+
+  void increment() {
+    state++;
+  }
+}
+
+class CounterWidget extends ConsumerWidget {
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    // 1. watch the provider and rebuild when the value changes
+    final counter = ref.watch(counterProvider);
+    return ElevatedButton(
+      // 2. use the value
+      child: Text('Value: $counter'),
+      // 3. change the state inside a button callback
+      onPressed: () => ref.read(counterProvider.notifier).increment(),
+    );
+  }
+}
