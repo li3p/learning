44d5511dfab4115ee0e6918fd0164c6c2973d9ae
li3p <li3p@qq.com>
2023-11-26 10:06:04 +0800

    riverpod annotation

--- flutter/counter_riverpod/lib/main.dart
+++ flutter/counter_riverpod/lib/main.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  // runApp(const ProviderScope(child: MyApp()));
-  runApp(const ProviderScope(child: MyRiverpodApp()));
+  runApp(const ProviderScope(
+    // child: MyApp(),
+    child: MyRiverpodApp(),
+  ));
 }
